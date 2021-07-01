d171032573c8e5d27d42c20f368ee769ff4f989f
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-01 21:52:19 +0800

    done travel app ui with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,192 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TravelApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TravelApp extends StatelessWidget {
+  const TravelApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.pink,
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "images/plane.png",
+                    width: 320.0,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Text(
+                    "travel & relax".toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 10.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.location_city_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            height: 2.0,
+                            width: 20.0,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              print("Stays");
+                            },
+                            child: Text(
+                              "stays".toUpperCase(),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.white,
+                              side: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                              minimumSize: Size(
+                                200.0,
+                                40.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.airplanemode_active_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              print("Flights");
+                            },
+                            child: Text(
+                              "flights".toUpperCase(),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.white,
+                              side: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                              minimumSize: Size(
+                                200.0,
+                                40.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.car_rental_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              print("cars");
+                            },
+                            child: Text(
+                              "cars".toUpperCase(),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.white,
+                              side: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                              minimumSize: Size(
+                                200.0,
+                                40.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.car_rental_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              print("all-inclusive vacations");
+                            },
+                            child: Text(
+                              "all-inclusive vacations".toUpperCase(),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.pink,
+                              side: BorderSide(
+                                color: Colors.white,
+                                width: 2.0,
+                              ),
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12.0,
+                              ),
+                              backgroundColor: Colors.white,
+                              minimumSize: Size(
+                                200.0,
+                                40.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
